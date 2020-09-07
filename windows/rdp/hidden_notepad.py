f75bbd90fd1aa24517d937bc5cf98471fe604d4c
fuck <fuck@fuck.com>
2020-09-07 19:43:21 +0800

    fuck

--- windows/rdp/hidden_notepad.py
+++ windows/rdp/hidden_notepad.py
@@ -1,18 +1,21 @@
 import subprocess
 import win32gui
 import time
+import win32process
 
-proc = subprocess.Popen(["notepad.exe"])
+proc = subprocess.Popen(["notepad.exe"],shell=False)
 # lets wait a bit to app to start
 time.sleep(3)
+target = proc.pid
 
 def enumWindowFunc(hwnd, windowList):
     """ win32gui.EnumWindows() callback """
-    text = win32gui.GetWindowText(hwnd)
-    className = win32gui.GetClassName(hwnd)
+#    text = win32gui.GetWindowText(hwnd)
+#    className = win32gui.GetClassName(hwnd)
+    TId, PId = win32process.GetWindowThreadProcessId(hwnd)
     #print hwnd, text, className
-    if text.find("Notepad") >= 0:
-        windowList.append((hwnd, text, className))
+    if PId == target:
+        windowList.append(hwnd)
 
 myWindows = []
 # enumerate thru all top windows and get windows which are ours
@@ -20,7 +23,7 @@ win32gui.EnumWindows(enumWindowFunc, myWindows)
 
 # now hide my windows, we can actually check process info from GetWindowThreadProcessId
 # http://msdn.microsoft.com/en-us/library/ms633522(VS.85).aspx
-for hwnd, text, className in myWindows:
+for hwnd in myWindows:
     print hwnd,type(hwnd)
     win32gui.ShowWindow(hwnd, False)
 
