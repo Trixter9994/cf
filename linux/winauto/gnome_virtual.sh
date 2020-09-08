eb4ffbc8d546eb0eac5d646ed9066c6cb8f63abf
root <root@localhost.localdomain>
2020-09-08 14:51:53 -0500

    fuck

--- linux/winauto/gnome_virtual.sh
+++ linux/winauto/gnome_virtual.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -rf /dev/shm/xvfb
 mkdir /dev/shm/xvfb
-xvfb-run -f ~/.Xauthority -n 1 -s "-pixdepths 3 27 -fbdir /dev/shm/xvfb" xterm
+xvfb-run -f ~/.Xauthority -n 7 -s "-pixdepths 3 27 -fbdir /dev/shm/xvfb" gnome-shell --replace & echo xvfb_session_pid $!
+#mwm -display :7
 #xvfb-run -f ~/.Xauthority -n 1 -s "-pixdepths 3 27 -fbdir /dev/shm/xvfb -listen tcp " xterm
